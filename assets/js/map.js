dfc6764773856d96c195d68f56859d33eff8cb21
Padraic Doyle <padraic@padraicdoyle.com>
2018-11-30 16:59:21 -0500

    add drift

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         lng: -73.687746,
         title: 'Address',
         infoWindow: {
-            content: '<h5 class="title">Inspect Point</h5><p><span class="region">1915 5th Ave, Troy NY</span><br><span class="postal-code">12180</span><br><span class="country-name">United States</span></p>'
+            content: '<h5 class="title">Inspect Point</h5><p><span class="region">24 4th St, Troy NY</span><br><span class="postal-code">12180</span><br><span class="country-name">United States</span></p>'
         }
     });
 
